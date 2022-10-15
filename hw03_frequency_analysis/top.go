03612f153f3fd717366f4664d8910d1a8cba5da0
maraero <themaraero@gmail.com>
2022-10-15 20:27:39 +0300

    implement Top10 func

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,47 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const maxNumber = 10
+
+func Top10(input string) []string {
+	words := strings.Fields(input)
+	freqByWords := make(map[string]int)
+
+	for _, word := range words {
+		freqByWords[word]++
+	}
+
+	wordsByFreq := make(map[int][]string)
+
+	for word, freq := range freqByWords {
+		wordsByFreq[freq] = append(wordsByFreq[freq], word)
+	}
+
+	freqList := []int{}
+
+	for freq := range wordsByFreq {
+		freqList = append(freqList, freq)
+	}
+
+	sort.Slice(freqList, func(i, j int) bool { return freqList[i] > freqList[j] })
+	result := []string{}
+
+	for _, freq := range freqList {
+		words = wordsByFreq[freq]
+		sort.Slice(words, func(i, j int) bool { return words[i] < words[j] })
+
+		for _, word := range words {
+			result = append(result, word)
+
+			if len(result) == maxNumber {
+				return result
+			}
+		}
+	}
+
+	return result
 }
