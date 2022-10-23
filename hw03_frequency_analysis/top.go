2e7cf892c3c89abcb2c5674786015f8ec2237080
maraero <themaraero@gmail.com>
2022-10-23 16:58:31 +0300

    eject method calcWords

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -7,13 +7,19 @@ import (
 
 const maxNumber = 10
 
-func Top10(input string) []string {
-	words := strings.Fields(input)
+func calcWords(text string) map[string]int {
+	words := strings.Fields(text)
 	freqByWords := make(map[string]int)
 
 	for _, word := range words {
 		freqByWords[word]++
 	}
+
+	return freqByWords
+}
+
+func Top10(input string) []string {
+	freqByWords := calcWords(input)
 
 	wordsByFreq := make(map[int][]string)
 
@@ -31,7 +37,7 @@ func Top10(input string) []string {
 	result := []string{}
 
 	for _, freq := range freqList {
-		words = wordsByFreq[freq]
+		words := wordsByFreq[freq]
 		sort.Slice(words, func(i, j int) bool { return words[i] < words[j] })
 
 		for _, word := range words {
