e643ffb2c860ecb7046769aa28141c7f84fa48cc
maraero <themaraero@gmail.com>
2022-10-23 17:36:18 +0300

    split Top10 method into separate methods

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,12 +1,17 @@
 package hw03frequencyanalysis
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 )
 
 const maxNumber = 10
+
+func Top10(input string) []string {
+	freqByWords := calcWords(input)
+	wordFreqPairs := getSortedWordList(freqByWords)
+	return getMostFrequentWordsByLimit(wordFreqPairs, maxNumber)
+}
 
 func calcWords(text string) map[string]int {
 	words := strings.Fields(text)
@@ -24,42 +29,34 @@ type WordFreqPair struct {
 	word string
 }
 
-func Top10(input string) []string {
-	freqByWords := calcWords(input)
-	wordFreqPairs := make([]WordFreqPair, len(freqByWords))
+type FrequencyComparator []WordFreqPair
+
+func (a FrequencyComparator) Len() int {return len(a)}
+func (a FrequencyComparator) Swap(i, j int) {a[i], a[j] = a[j], a[i]}
+func (a FrequencyComparator) Less(i, j int) bool {
+	if a[i].freq == a[j].freq {
+		return a[i].word > a[j].word
+	}
+
+
+	return a[i].freq < a[j].freq
+}
+
+func getSortedWordList(freqByWords map[string]int) []WordFreqPair {
+	wordFreqPairs := []WordFreqPair{}
 	for word, freq := range freqByWords {
 		pair := WordFreqPair{freq: freq, word: word}
 		wordFreqPairs = append(wordFreqPairs, pair)
 	}
-	fmt.Println(wordFreqPairs)
+	sort.Sort(FrequencyComparator(wordFreqPairs))
+	return wordFreqPairs
+}
 
-	wordsByFreq := make(map[int][]string)
-
-	for word, freq := range freqByWords {
-		wordsByFreq[freq] = append(wordsByFreq[freq], word)
-	}
-
-	freqList := []int{}
-
-	for freq := range wordsByFreq {
-		freqList = append(freqList, freq)
-	}
-
-	sort.Slice(freqList, func(i, j int) bool { return freqList[i] > freqList[j] })
+func getMostFrequentWordsByLimit(list []WordFreqPair, limit int) []string {
 	result := []string{}
-
-	for _, freq := range freqList {
-		words := wordsByFreq[freq]
-		sort.Slice(words, func(i, j int) bool { return words[i] < words[j] })
-
-		for _, word := range words {
-			result = append(result, word)
-
-			if len(result) == maxNumber {
-				return result
-			}
-		}
+	for i := len(list) - 1; i >= 0 && limit > 0; i-- {
+		result = append(result, list[i].word)
+		limit--
 	}
-
 	return result
 }
