a2dc8673031c7dc84f7fe54475a0b123ebda2e18
maraero <themaraero@gmail.com>
2022-10-29 13:35:20 +0300

    complete parallel executor

---
+++ hw05_parallel_execution/err_counter.go
@@ -0,0 +1,29 @@
+package hw05parallelexecution
+
+import "sync"
+
+type errCounter struct {
+	mu      sync.Mutex
+	counter int
+	limit int
+}
+
+func (c *errCounter) inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.counter++
+}
+
+func (c *errCounter) exceedsLimit() bool {
+	if c.limit == 0 {
+		return false
+	}
+
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.counter >= c.limit
+}
+
+func NewErrCouner(limit int) *errCounter {
+	return &errCounter{limit: limit}
+}
--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,14 +2,43 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
+	ch := make(chan Task)
+	wg := sync.WaitGroup{}
+	errCounter := NewErrCouner(m)
+
+	wg.Add(n)
+	for i := 0; i < n; i++ {
+		go func() {
+			defer wg.Done()
+			for task := range ch {
+				if err := task(); err != nil {
+					errCounter.inc()
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		if errCounter.exceedsLimit() {
+			break;
+		}
+		ch <- task
+	}
+
+	close(ch)
+	wg.Wait()
+
+	if errCounter.exceedsLimit() {
+		return ErrErrorsLimitExceeded
+	}
+
 	return nil
 }
