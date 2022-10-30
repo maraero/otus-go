39e7f39de083674cd498087027158aa1deed5a55
maraero <themaraero@gmail.com>
2022-10-30 11:17:18 +0300

    add comment to Run function

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -9,6 +9,9 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
+// Run executes functions in n goroutines.
+// If the number of errors reaches `m`, no more than `m+n` functions will be executed.
+// If `m == 0` all functions will be executed in spite of errors
 func Run(tasks []Task, n, m int) error {
 	ch := make(chan Task)
 	wg := sync.WaitGroup{}
