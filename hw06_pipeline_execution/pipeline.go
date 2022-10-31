ad5f950682a7f4c16bdb4060e0fe1a60decb35a6
maraero <themaraero@gmail.com>
2022-10-31 10:10:41 +0300

    complete ExecutePipeline func

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,37 @@ type (
 
 type Stage func(in In) (out Out)
 
+func interrupter(in In, done In) Out {
+	out := make(Bi)
+
+	go func() {
+		defer func() {
+			close(out)
+			for range in {}
+		}()
+
+		for {
+			select {
+			case v, ok := <- in:
+				if !ok {
+					return
+				}
+				out <- v
+			case <- done:
+				return
+			}
+		}
+	}()
+
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	var out = in
+
+	for _, stage := range stages {
+		out = stage(interrupter(out, done))
+	}
+
+	return out
 }
