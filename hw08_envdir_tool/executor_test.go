446aed3ff3e1f921c5ceff80110857c9c2f706ce
maraero <themaraero@gmail.com>
2022-11-20 19:45:05 +0300

    move env_reader test

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -2,28 +2,8 @@ package main
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/require"
 )
 
 func TestRunCmd(t *testing.T) {
-	t.Run("read", func(t *testing.T) {
-		expected := Environment{
-			"BAR":   {Value: "bar", NeedRemove: false},
-			"EMPTY": {Value: "", NeedRemove: true},
-			"FOO":   {Value: "   foo\nwith new line", NeedRemove: false},
-			"HELLO": {Value: "\"hello\"", NeedRemove: false},
-			"UNSET": {Value: "", NeedRemove: true},
-		}
-		env, err := ReadDir("./testdata/env")
-		require.NoError(t, err)
-		require.Equal(t, expected, env)
-	})
 
-	t.Run("path not found", func(t *testing.T) {
-		var expected Environment
-		env, err := ReadDir("./testdata/missing_dir")
-		require.Error(t, err)
-		require.Equal(t, expected, env)
-	})
 }
