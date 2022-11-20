72c38a1bb92b251c8129695c659313edf7433310
maraero <themaraero@gmail.com>
2022-11-20 19:44:04 +0300

    add base executor

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,55 @@
 package main
 
+import (
+	"os"
+	"os/exec"
+)
+
+const (
+	exitSuccess = 0
+	exitError   = 1
+)
+
+func handleEnvs(env Environment) error {
+	for name, val := range env {
+		var err error
+
+		if val.NeedRemove == true {
+			err = os.Unsetenv(name)
+		} else {
+			err = os.Setenv(name, val.Value)
+		}
+
+		return err
+	}
+
+	return nil
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return exitError
+	}
+
+	if err := handleEnvs(env); err != nil {
+		return exitError
+	}
+
+	c := exec.Command(cmd[0], cmd[1:]...)
+	c.Env = os.Environ()
+	c.Stderr = os.Stderr
+	c.Stdin = os.Stdin
+	c.Stdout = os.Stdout
+
+	err := c.Run()
+
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		return exitErr.ExitCode()
+	}
+	if err != nil {
+		return exitError
+	}
+
+	return exitSuccess
 }
