848401e53a707279424c56b76b372cda9845d0b0
maraero <themaraero@gmail.com>
2022-11-27 12:09:20 +0300

    implement method Error for ValidationErrors

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,5 +1,10 @@
 package hw09structvalidator
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ValidationError struct {
 	Field string
 	Err   error
@@ -8,7 +13,11 @@ type ValidationError struct {
 type ValidationErrors []ValidationError
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var res strings.Builder
+	for _, err := range v {
+		res.WriteString(fmt.Sprintf("field \"%v\" error: \"%v\"", err.Field, err.Err))
+	}
+	return res.String()
 }
 
 func Validate(v interface{}) error {
