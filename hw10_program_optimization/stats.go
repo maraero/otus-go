e298535af42f97e87acdcbe75f0113b7bb1550da
maraero <themaraero@gmail.com>
2022-12-04 23:00:16 +0300

    revert commits for hw10

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,11 +1,12 @@
 package hw10programoptimization
 
 import (
-	"bufio"
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"regexp"
 	"strings"
-
-	jsoniter "github.com/json-iterator/go"
 )
 
 type User struct {
@@ -21,22 +22,45 @@ type User struct {
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	var parser = jsoniter.ConfigCompatibleWithStandardLibrary
+	u, err := getUsers(r)
+	if err != nil {
+		return nil, fmt.Errorf("get users error: %w", err)
+	}
+	return countDomains(u, domain)
+}
+
+type users [100_000]User
+
+func getUsers(r io.Reader) (result users, err error) {
+	content, err := ioutil.ReadAll(r)
+	if err != nil {
+		return
+	}
+
+	lines := strings.Split(string(content), "\n")
+	for i, line := range lines {
+		var user User
+		if err = json.Unmarshal([]byte(line), &user); err != nil {
+			return
+		}
+		result[i] = user
+	}
+	return
+}
+
+func countDomains(u users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
-	ending := "." + domain
 
-	bScanner := bufio.NewScanner(r)
-
-	var user User
-	for bScanner.Scan() {
-		err := parser.Unmarshal(bScanner.Bytes(), &user)
+	for _, user := range u {
+		matched, err := regexp.Match("\\."+domain, []byte(user.Email))
 		if err != nil {
 			return nil, err
 		}
-		email := strings.ToLower(user.Email)
-		if strings.HasSuffix(email, ending) {
-			fDomain := strings.SplitN(email, "@", 2)[1]
-			result[fDomain]++
+
+		if matched {
+			num := result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])]
+			num++
+			result[strings.ToLower(strings.SplitN(user.Email, "@", 2)[1])] = num
 		}
 	}
 	return result, nil
