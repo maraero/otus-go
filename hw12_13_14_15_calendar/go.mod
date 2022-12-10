009d39a6fab0ab17ed5e2bd253ede88617f63dc2
maraero <themaraero@gmail.com>
2022-12-10 15:29:28 +0300

    add base of event service

--- hw12_13_14_15_calendar/go.mod
+++ hw12_13_14_15_calendar/go.mod
@@ -3,6 +3,7 @@ module github.com/maraero/otus-go/hw12_13_14_15_calendar
 go 1.16
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
---
+++ hw12_13_14_15_calendar/internal/storage/event_interface.go
@@ -0,0 +1,14 @@
+package storage
+
+import "time"
+
+type Event struct {
+	ID               string
+	Title            string
+	DateStart        time.Time
+	DateEnd          time.Time
+	Descripion       string
+	UserId           string
+	DateNotification time.Time
+	deleted          bool
+}
