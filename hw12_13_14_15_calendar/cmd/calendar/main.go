2d33eec1a07a85a808e6e59de51c7851567bde8e
maraero <themaraero@gmail.com>
2022-12-28 18:09:20 +0300

    update main file

--- hw12_13_14_15_calendar/cmd/calendar/main.go
+++ hw12_13_14_15_calendar/cmd/calendar/main.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/maraero/otus-go/hw12_13_14_15_calendar/internal/app"
 	"github.com/maraero/otus-go/hw12_13_14_15_calendar/internal/config"
 	es "github.com/maraero/otus-go/hw12_13_14_15_calendar/internal/event-service/service"
@@ -41,53 +43,73 @@ func main() {
 		log.Fatal(err)
 	}
 
-	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	ctx, cancel := context.WithCancel(context.Background())
+	go watchSignals(cancel)
 	defer cancel()
 
+	dbConnection := connectDB(ctx, logger, config)
+	eventService := es.New(dbConnection)
+	calendar := app.New(eventService, logger)
+	httpServer := serverhttp.New(calendar, config.Server)
+	grpcServer := servergrpc.New(calendar, config.Server)
+
+	logger.Info("calendar is running...")
+	<-ctx.Done()
+	logger.Info("calendar is turning off...")
+	shutDown(dbConnection, httpServer, grpcServer, logger)
+	logger.Info("calendar stopped")
+}
+
+func watchSignals(cancel context.CancelFunc) {
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+
+	<-signals
+	cancel()
+}
+
+func connectDB(ctx context.Context, logger *l.Log, config config.Config) *sqlx.DB {
 	dbConnection := newDBConnection(ctx, logger, config.Storage)
-	defer func() {
-		if dbConnection != nil {
-			err := dbConnection.Close()
-			if err != nil {
-				log.Fatal("can not close database connection: %w", err)
-			}
-		}
-	}()
 
 	if dbConnection != nil {
 		migrate(dbConnection.DB, logger)
 	}
 
-	eventService := es.New(dbConnection)
-	calendar := app.New(eventService, logger)
+	return dbConnection
+}
 
-	httpServer := serverhttp.New(calendar, config.Server)
-	grpcServer := servergrpc.New(calendar, config.Server)
+func shutDown(dbConnection *sqlx.DB, httpServer *serverhttp.Server, grpcServer *servergrpc.Server, logger *l.Log) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
 	go func() {
-		<-ctx.Done()
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
-		defer cancel()
+		defer wg.Done()
+		if dbConnection != nil {
+			err := dbConnection.Close()
+			if err != nil {
+				logger.Error("can not close database connection: %w", err)
+			}
+		}
+	}()
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 		if err := httpServer.Stop(ctx); err != nil {
 			logger.Error("failed to stop http server: " + err.Error())
 		}
+	}()
 
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
 		if err := grpcServer.Stop(); err != nil {
 			logger.Error("failed to stop grpc server: " + err.Error())
 		}
 	}()
 
-	logger.Info("calendar is running...")
-
-	if err := httpServer.Start(); err != nil {
-		logger.Error("failed to start http server: " + err.Error())
-		cancel()
-		os.Exit(1) //nolint:gocritic
-	}
-
-	if err = grpcServer.Start(); err != nil {
-		logger.Error("failed to start grpc server: " + err.Error())
-		cancel()
-		os.Exit(1)
-	}
+	wg.Wait()
 }
--- hw12_13_14_15_calendar/todo.md
+++
@@ -1,4 +0,0 @@
-# TODOs
-
-- try to close app without errors
-- send PR for check
